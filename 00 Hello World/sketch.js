ac3d7fc8fe1049d8d469c77d63beb268c0b6f7dd
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-09 11:24:46 +0200

    initial

---
+++ 00 Hello World/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+  
+  let greeting = 'He' + 'llo';
+  let x = 24;
+  let y = 96;
+  y = y + 100;
+
+  text(x + ',' + y, x, y);
+  
+  y = y + 20;
+  text(x + ',' + y, x, y);
+  
+  y = y + 100;
+  text(x + ',' + y, x, y);
+}
