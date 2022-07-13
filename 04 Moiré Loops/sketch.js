f58db24a74235962c08b5c00369097db1293343e
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-13 13:07:07 +0200

    add moiré example

---
+++ 04 Moiré Loops/sketch.js
@@ -0,0 +1,18 @@
+let x1 = 0;
+let y1 = 10;
+let x2 = 0;
+let y2 = 10;
+
+function setup() {
+    createCanvas(540, 540);
+    background(250);
+    strokeWeight(0.25); // skinny lines
+}
+
+function draw() {    
+    line(x1, y1, x2, y2);
+    
+    x2 = (x2 + 1) % width;
+    y1 = (y1 + 1) % height;
+    y2 = (y2 + 1.2) % height;
+}
