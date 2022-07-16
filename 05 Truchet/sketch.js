217fc20b3786f6d87b2e790808152a0e375e7c3c
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-16 07:42:34 +0200

    adjust matrix, add random rotation

--- 05 Truchet/sketch.js
+++ 05 Truchet/sketch.js
@@ -5,15 +5,22 @@ let size = 32; // size of tile
 function setup() {
     createCanvas(countX * size, countY * size);
     background(250);
+    angleMode(DEGREES);
 
     for(let x=0; x<countX; x++) {
-        push();
         for(let y=0; y<countY; y++) {
+            push();
+            
+            translate(x * size, y * size);
+            
+            translate(size / 2, size / 2);
+            rotate(random([0, 90, 180, 270]));
+            translate(-size / 2, -size / 2);
+
             square(0, 0, size);
-            square(0, 0, size/4);
-            translate(0, size);
+            square(0, 0, size / 4);
+            
+            pop();
         }
-        pop();
-        translate(size, 0);
     }
 }
