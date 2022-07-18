5f6c17559f42e8f58efda0931d0060b82e523cc0
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-18 16:39:42 +0200

    tweak order

--- 08 Matrix Returns/sketch.js
+++ 08 Matrix Returns/sketch.js
@@ -6,19 +6,21 @@ function setup() {
 function draw() {
     background(250);
     let angle = frameCount % 360;
-    let count = 20;
-    let span = 20;
+    let count = 10;
+    let span = 120;
 
     translate(width / 2, height / 2);
 
     for (let i = 0; i < count; i++) {
-        let stepAngle = angle / (i + 1);
-        translate(span, 0);
-        rotate(stepAngle);
+        let stepSpan = span / (i + 1);
+        circle(0, 0, i);
 
-        circle(0, 0, 10);
-        line(0, 0, span, 0);
-        text(round(stepAngle), 4, -4);
+        rotate(angle);
+
+        circle(0, 0, i + 1);
+        line(0, 0, stepSpan, 0);
+        text(angle, 4, -4);
+        translate(stepSpan, 0);
     }
-    circle(span, 0, 10);
+    circle(0, 0, 10);
 }
