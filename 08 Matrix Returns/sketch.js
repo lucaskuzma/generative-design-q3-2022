fed5dd53d788225a1aa40d448a062e6de0e7e956
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-18 16:47:42 +0200

    draw less circles

--- 08 Matrix Returns/sketch.js
+++ 08 Matrix Returns/sketch.js
@@ -13,14 +13,13 @@ function draw() {
 
     for (let i = 0; i < count; i++) {
         let stepSpan = span / (i + 1);
-        circle(0, 0, i);
 
         rotate(angle);
 
-        circle(0, 0, i + 1);
+        circle(stepSpan, 0, i * 2);
         line(0, 0, stepSpan, 0);
         text(angle, 4, -4);
+
         translate(stepSpan, 0);
     }
-    circle(0, 0, 10);
 }
