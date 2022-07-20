27798ec43a451924344a35ae2b278faa3166b41d
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-20 15:40:13 +0200

    add color

--- 09 Recursion/sketch.js
+++ 09 Recursion/sketch.js
@@ -8,11 +8,11 @@ function draw() {
     halve(0, 0, 400, 400);
 }
 
-function halve(x, y, w, h, depth = 0) {
+function halve(x, y, w, h, depth = 0, g = 128, b = 128) {
     let limit = 5;
     if (depth > 20) return;
 
-    fill(depth * 10);
+    fill(depth * 10, g, b);
     stroke(255);
     rect(x, y, w, h);
 
@@ -20,14 +20,14 @@ function halve(x, y, w, h, depth = 0) {
     if (random() < 0.5) {
         let nw = random(w);
         if (nw > limit && nw < w - limit) {
-            halve(x, y, nw, h, depth + 1);
-            halve(x + nw, y, w - nw, h, depth + 1);
+            halve(x, y, nw, h, depth + 1, g - 10, b);
+            halve(x + nw, y, w - nw, h, depth + 1, g + 10, b);
         }
     } else {
         let nh = random(h);
         if (nh > limit && nh < h - limit) {
-            halve(x, y, w, nh, depth + 1);
-            halve(x, y + nh, w, h - nh, depth + 1);
+            halve(x, y, w, nh, depth + 1, g, b - 10);
+            halve(x, y + nh, w, h - nh, depth + 1, g, b + 10);
         }
     }
 }
