85e84f19c38305581be884214ae48bebcc5b856e
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-29 13:29:00 +0200

    add 3d example

---
+++ 14 Three Dimensions/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
+    background(220);
+    noStroke();
+}
+
+function draw() {
+    pointLight(250, 250, 250, 0, -height / 2, 350);
+    ambientLight(150);
+
+    rotate(frameCount);
+    translate(width / 4, 0);
+    rotate(frameCount * 2);
+
+    specularMaterial(100 + sin(frameCount) * 100, 100, 100);
+    shininess(1x0);
+
+    box(60, 30, 10);
+}
