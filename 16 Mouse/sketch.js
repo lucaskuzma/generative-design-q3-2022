d5aedc3f801b57d3f2a0cbd51779c7671d10b487
Lucas Kuzma <lucas@lucaskuzma.com>
2022-07-30 07:47:48 +0200

    mouse interaction

---
+++ 16 Mouse/sketch.js
@@ -0,0 +1,25 @@
+let down = false;
+
+function setup() {
+    createCanvas(400, 400);
+    noStroke();
+    fill(100);
+}
+
+function draw() {
+    if (down) {
+        rect(mouseX, mouseY, 2, 50);
+    }
+}
+
+function mousePressed() {
+    down = true;
+}
+
+function mouseReleased() {
+    down = false;
+}
+
+function doubleClicked() {
+    background(255);
+}
